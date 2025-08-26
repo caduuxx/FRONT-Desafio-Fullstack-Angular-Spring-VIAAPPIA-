41746c52de48d69f7e1087415bace6481f2c8edb
caduuxx <cbcadulebe@gmail.com>
2025-08-26 18:18:38 -0300

    feat : paginaçao e detalhes do incident

--- src/app/features/incidents/incident-detail.component.ts
+++ src/app/features/incidents/incident-detail.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { IncidentsService } from '../../core/services/incidents.service';
 import { FormsModule } from '@angular/forms';
+import { Status } from '../../core/models/enums';
 
 @Component({
   selector: 'app-incident-detail',
@@ -14,19 +15,52 @@ import { FormsModule } from '@angular/forms';
 export class IncidentDetailComponent implements OnInit {
   incident: any;
   newComment: string = '';
+  statusOptions = Object.values(Status); // Para dropdown de status
+  loading = false;
+  error = '';
 
   constructor(private route: ActivatedRoute, private incidentService: IncidentsService) {}
 
   ngOnInit() {
+    this.loadIncident();
+  }
+
+  loadIncident() {
     const id = this.route.snapshot.paramMap.get('id')!;
-    this.incidentService.getIncidentById(id).subscribe(res => this.incident = res);
+    this.loading = true;
+    this.incidentService.getIncidentById(id).subscribe({
+      next: (res) => {
+        this.incident = res;
+        this.loading = false;
+      },
+      error: () => {
+        this.error = 'Erro ao carregar incidente.';
+        this.loading = false;
+      }
+    });
   }
 
   addComment() {
     if (!this.newComment) return;
-    this.incidentService.addComment(this.incident.id, this.newComment, 'Usuário').subscribe(res => {
-      this.incident.comments.push(res);
-      this.newComment = '';
+    this.incidentService.addComment(this.incident.id, this.newComment, 'Usuário').subscribe({
+      next: (res) => {
+        if (!this.incident.comments) this.incident.comments = [];
+        this.incident.comments.push(res);
+        this.newComment = '';
+      },
+      error: () => {
+        this.error = 'Erro ao adicionar comentário.';
+      }
+    });
+  }
+
+  changeStatus(newStatus: Status) {
+    if (newStatus === this.incident.status) return;
+    this.incidentService.changeStatus(this.incident.id, newStatus).subscribe({
+      next: (res) => this.incident.status = res.status,
+      error: () => {
+        this.error = 'Erro ao alterar status.';
+      }
     });
   }
 }
--- src/app/features/incidents/list-incidents.component.ts
+++ src/app/features/incidents/list-incidents.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IncidentsService } from '../../core/services/incidents.service';
+import { Status, Priority } from '../../core/models/enums';
 import { DateFormatPipe } from '../../shared/pipes/date-format.pipe';
-import { normalizeTags, buildQueryParams } from '../../shared/utils/form-utils';
+import { buildQueryParams } from '../../shared/utils/form-utils';
 
 @Component({
   selector: 'app-list-incidents',
@@ -14,8 +15,19 @@ import { normalizeTags, buildQueryParams } from '../../shared/utils/form-utils';
 export class ListIncidentsComponent implements OnInit {
   incidents: any[] = [];
   loading = false;
+  error = '';
 
-  constructor(private incidentService: IncidentsService) {}
+  // filtros
+  statusFilter: string = '';
+  priorityFilter: string = '';
+  searchQuery: string = '';
+
+  // paginação
+  page = 0;
+  size = 10;
+  totalElements = 0;
+
+  constructor(private incidentsService: IncidentsService) {}
 
   ngOnInit() {
     this.fetchIncidents();
@@ -23,17 +35,44 @@ export class ListIncidentsComponent implements OnInit {
 
   fetchIncidents() {
     this.loading = true;
-    const page = 0;
-    const size = 10;
-    this.incidentService.getIncidents(page, size).subscribe({
-      next: (res: any) => {
-        this.incidents = res.content; // porque o serviço retorna { content, totalElements }
-        this.loading = false;
-      },
-      error: () => {
-        this.loading = false;
-        console.error('Erro ao carregar incidentes');
-      }
-    });
+    this.error = '';
+
+    // converte strings do select para enums
+    const statusEnum = this.statusFilter ? (Status as any)[this.statusFilter] : undefined;
+    const priorityEnum = this.priorityFilter ? (Priority as any)[this.priorityFilter] : undefined;
+
+    this.incidentsService
+      .getIncidents(this.page, this.size, statusEnum, priorityEnum, this.searchQuery)
+      .subscribe({
+        next: (res: any) => {
+          this.incidents = res.content;
+          this.totalElements = res.totalElements;
+          this.loading = false;
+        },
+        error: () => {
+          this.error = 'Erro ao carregar incidentes.';
+          this.loading = false;
+        }
+      });
+  }
+
+  // métodos de paginação
+  goToPage(page: number) {
+    this.page = page;
+    this.fetchIncidents();
+  }
+
+  nextPage() {
+    if ((this.page + 1) * this.size < this.totalElements) {
+      this.page++;
+      this.fetchIncidents();
+    }
+  }
+
+  previousPage() {
+    if (this.page > 0) {
+      this.page--;
+      this.fetchIncidents();
+    }
   }
 }
